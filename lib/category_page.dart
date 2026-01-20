ca4d85cc309150406a0b000b80e0834e6de11243
Marlon Mestica <mmestica@gmail.com>
2026-01-20 17:37:59 +0200

    - additional phrases - category icon on top bar

--- lib/category_page.dart
+++ lib/category_page.dart
@@ -9,17 +9,32 @@ class CategoryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(category.title)),
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Icon(category.icon, size: 48, color: Colors.blue),
+            SizedBox(width: 10),
+            Text(category.title),
+          ],
+        ),
+      ),
       body: ListView.separated(
         itemCount: category.phrases.length,
         separatorBuilder: (_, __) => const Divider(),
         itemBuilder: (context, index) {
           final phrase = category.phrases[index];
           return ListTile(
-            // leading: const Icon(Icons.volume_up),
-            title: Text(phrase.english, style: const TextStyle(fontSize: 18)),
+            dense: true,
+            visualDensity: const VisualDensity(
+              vertical: -4, // minimum practical value
+            ),
+            contentPadding: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 0,
+            ),
+            title: Text(phrase.finnish, style: const TextStyle(fontSize: 18)),
             subtitle: Text(
-              phrase.finnish,
+              phrase.english,
               style: const TextStyle(fontSize: 16, color: Colors.blueGrey),
             ),
           );
